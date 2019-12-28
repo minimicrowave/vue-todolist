06ce523f22d3af6e42ceb63627aec5633389198c
Serene <unexpecte-d@hotmail.com>
2019-12-28 18:19:36 +0800

    Inital commit with basic functionalities

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,60 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <AddTodo v-on:add-todo="addTodo" />
+    <Todos v-bind:todos="todos" v-on:del-todo="deleteTodo" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from "./components/layout/Header";
+import Todos from "./components/Todos";
+import AddTodo from "./components/AddTodo";
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
+  name: "app",
+  components: { Header, Todos, AddTodo },
+  data() {
+    return {
+      todos: [
+        { id: 1, title: "One", completed: false },
+        { id: 2, title: "Two", completed: true },
+        { id: 3, title: "Three", completed: false }
+      ]
+    };
+  },
+  methods: {
+    deleteTodo(id) {
+      this.todos = this.todos.filter(todo => todo.id !== id);
+    },
+    addTodo(newTodo) {
+      this.todos = [...this.todos, newTodo];
+    }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+* {
+  box-sizing: 0;
+  padding: 0;
+}
+
+body {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  line-height: 1.4;
+}
+
+.btn {
+  display: inline-block;
+  border: none;
+  color: white;
+  background: #555;
+  padding: 7px 20px;
+  cursor: pointer;
+}
+
+.btn:hover {
+  background: #666;
 }
 </style>
---
+++ src/components/AddTodo.vue
@@ -0,0 +1,37 @@
+<template>
+  <div>
+    <form @submit="addTodo">
+      <input type="text" name="title" placeholder="Add Todo..." v-model="title" />
+      <input type="submit" value="Submit" class="btn" />
+    </form>
+  </div>
+</template>
+
+<script>
+import uuid from "uuid";
+
+export default {
+  name: "AddTodo",
+  data() {
+    return { title: "" };
+  },
+  methods: {
+    addTodo(e) {
+      e.preventDefault();
+      const newTodo = { id: uuid.v4(), title: this.title, completed: false };
+      this.$emit("add-todo", newTodo);
+    }
+  }
+};
+</script>
+
+<style scoped>
+form {
+  display: flex;
+}
+
+input[type="text"] {
+  flex: 10;
+  padding: 5px;
+}
+</style>
